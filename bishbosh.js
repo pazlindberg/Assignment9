019d5b0cf8e94c0760b0254102cea0c2f47a7d4e
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-04-21 14:05:43 +0200

    3

---
+++ bishbosh.js
@@ -0,0 +1,17 @@
+for(i=1;i<=100;i++)
+{
+	let s="";
+	if((i%3===0)&&(i%4===0))
+		s="Bish-Bosh";
+
+	else if(i%3===0)
+		s="Bish";
+	
+	else if(i%4===0)
+		s="Bosh";
+	
+	else
+		s=i;
+	
+	document.write(s + " ");
+}
