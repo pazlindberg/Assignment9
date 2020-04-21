e1052baba7b1eb1ce953aef168e08b63c390ff02
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-04-21 14:22:32 +0200

    6

--- bishbosh.js
+++ bishbosh.js
@@ -1,20 +1,32 @@
-function bishBosh()
+function bishBosh(bish,bosh,iterations)
 {
-	for(i=1;i<=100;i++)
+	if(bish<=0||bosh<=0||iterations<=0)
 	{
-		let s="";
-		if((i%3===0)&&(i%4===0))
-			s="Bish-Bosh";
-
-		else if(i%3===0)
-			s="Bish";
-		
-		else if(i%4===0)
-			s="Bosh";
-		
-		else
-			s=i;
-		document.getElementById("bishBoshOut").innerHTML += s+" ";
-		//document.write(s + " ");
+		document.getElementById("bishBoshOut").innerHTML = "invalid options!";
 	}
+	else
+	{
+		for(i=1;i<=iterations;i++)
+		{
+			let s="";
+			if((i%bish===0)&&(i%bosh===0))
+				s="Bish-Bosh";
+
+			else if(i%bish===0)
+				s="Bish";
+			
+			else if(i%bosh===0)
+				s="Bosh";
+			
+			else
+				s=i;
+			document.getElementById("bishBoshOut").innerHTML += s+" ";
+			//document.write(s + " ");
+		}
+	}
+}
+
+function bishBoshRun()
+{
+	bishBosh(1,2,20);
 }
